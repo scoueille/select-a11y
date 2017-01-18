7e2768ae0924b46e0d65a6de7a77b54306e648de
Laurent DUTHEIL <dutheil74@free.fr>
2017-01-18 18:05:35 +0100

    feat(SP-1436) create the list items on selection

--- spec/filter-component.spec.js
+++ spec/filter-component.spec.js
@@ -1,43 +1,42 @@
 /* global describe, expect, it, beforeEach */
 define(['../lib/filter-component'], function (filterComponent) {
-    describe("A suite is just a function", function () {
-        var a;
-        var fixture = setFixtures('' +
-            '<form action="" data-filter-component>' +
-            '  <div class="form-group">' +
-            '    <label for="select">Ajouter un filtre</label>' +
-            '    <select class="form-control" id="select" multiple>' +
-            '        <option>Option 1</option>' +
-            '        <option>Option 2</option>' +
-            '        <option>Option 3</option>' +
-            '        <option>Option 4</option>' +
-            '        <option>Option 5</option>' +
-            '    </select>' +
-            '  </div>' +
-            '  <p>' +
-            '    <button class="btn btn-primary" type="submit">Filtrer</button>' +
-            '  </p>' +
-            '</form>' +
-            '' +
-            '<form action="" class="form-i-must-not-tranform">' +
-            '  <div class="form-group">' +
-            '    <label for="select">Ajouter un filtre pas transformé</label>' +
-            '    <select class="form-control" id="select-i-must-not-transform" multiple>' +
-            '        <option>Option 1</option>' +
-            '        <option>Option 2</option>' +
-            '    </select>' +
-            '  </div>' +
-            '  <p>' +
-            '    <button class="btn btn-primary" type="submit">Filtrer</button>' +
-            '  </p>' +
-            '</form>'
-        );
+    describe("A filter component", function () {
+        var fixture;
 
         beforeEach(function () {
-            $('body').append(fixture);
+            fixture = setFixtures('' +
+                '<form action="" data-filter-component>' +
+                '  <div class="form-group">' +
+                '    <label for="select">Ajouter un filtre</label>' +
+                '    <select class="form-control" id="select" multiple>' +
+                '        <option>Option 1</option>' +
+                '        <option>Option 2</option>' +
+                '        <option>Option 3</option>' +
+                '        <option>Option 4</option>' +
+                '        <option>Option 5</option>' +
+                '    </select>' +
+                '  </div>' +
+                '  <p>' +
+                '    <button class="btn btn-primary" type="submit">Filtrer</button>' +
+                '  </p>' +
+                '</form>' +
+                '' +
+                '<form action="" class="form-i-must-not-tranform">' +
+                '  <div class="form-group">' +
+                '    <label for="select">Ajouter un filtre pas transformé</label>' +
+                '    <select class="form-control" id="select-i-must-not-transform" multiple>' +
+                '        <option>Option 1</option>' +
+                '        <option>Option 2</option>' +
+                '    </select>' +
+                '  </div>' +
+                '  <p>' +
+                '    <button class="btn btn-primary" type="submit">Filtrer</button>' +
+                '  </p>' +
+                '</form>'
+            );
         });
 
-        describe("Before transformation", function () {
+        describe("before transformation", function () {
             var formToTranform;
 
             beforeEach(function () {
@@ -65,16 +64,19 @@ define(['../lib/filter-component'], function (filterComponent) {
 
         });
 
-        describe("After transformation", function () {
+        describe("after transformation", function () {
             var tranformedForm;
+            var a11yDiv;
+            var hiddenDiv;
 
             beforeEach(function () {
                 filterComponent.transform();
                 tranformedForm = fixture.find('form[data-filter-component]');
+                hiddenDiv = tranformedForm.find('select').parent();
+                a11yDiv = hiddenDiv.prev();
             });
 
             it("should hide the inner div of the transformed form", function () {
-                var hiddenDiv = tranformedForm.find('select').parent();
                 expect(hiddenDiv).toBeHidden();
             });
 
@@ -83,13 +85,9 @@ define(['../lib/filter-component'], function (filterComponent) {
                 expect(divIMustNotTransform).not.toBeHidden();
             });
 
-            describe("A new div contains the a11y selector", function () {
-                var a11yDiv;
-                var hiddenDiv;
+            describe("a new div contains the a11y selector", function () {
 
                 beforeEach(function () {
-                    hiddenDiv = tranformedForm.find('div:hidden');
-                    a11yDiv = hiddenDiv.prev();
                 });
 
                 it("should add the div before the hidden div", function () {
@@ -98,7 +96,7 @@ define(['../lib/filter-component'], function (filterComponent) {
                     expect(a11yDiv).toBeMatchedBy('div');
                 });
 
-                it("should have a class row", function() {
+                it("should have a class row", function () {
                     expect(a11yDiv).toHaveClass('row');
                 });
 
@@ -110,23 +108,23 @@ define(['../lib/filter-component'], function (filterComponent) {
                     });
                 });
 
-                it("should have an input text", function () {
-                    var input = a11yDiv.find(':text');
-                    expect(input).toExist();
-                });
-
-                describe("The a11y input handles a datalist element", function () {
+                describe("the a11y input handles a datalist element", function () {
                     var input;
 
-                    beforeEach(function() {
+                    beforeEach(function () {
                         input = a11yDiv.find(':text');
                     });
 
-                    it("should have an id", function() {
+                    it("should have an input text", function () {
+                        var input = a11yDiv.find(':text');
+                        expect(input).toExist();
+                    });
+
+                    it("should have an id", function () {
                         expect(input).toHaveId('a11y-select-js');
                     });
 
-                    it("should have a class js-combobox", function() {
+                    it("should have a class js-combobox", function () {
                         expect(input).toHaveClass('js-combobox');
                     });
 
@@ -139,7 +137,7 @@ define(['../lib/filter-component'], function (filterComponent) {
                         });
                     });
 
-                    it("should have an attribut 'list'", function() {
+                    it("should have an attribut 'list'", function () {
                         expect(input).toHaveAttr('list', 'a11y-select-option-list');
                     });
 
@@ -150,10 +148,10 @@ define(['../lib/filter-component'], function (filterComponent) {
                         expect(label).toHaveAttr('for', 'a11y-select-js');
                     });
 
-                    describe("The datalist is filled with the hidden select options", function() {
+                    describe("The datalist is filled with the hidden select options", function () {
                         var datalist;
 
-                        beforeEach(function() {
+                        beforeEach(function () {
                             datalist = a11yDiv.find('datalist');
                         });
 
@@ -165,28 +163,135 @@ define(['../lib/filter-component'], function (filterComponent) {
                             expect(datalist).toHaveId('a11y-select-option-list');
                         });
 
-                        it("should be filled with the existing hidden select options", function () {
-                            var datalistOptions = datalist.find('option');
-                            var hiddenSelectOptions = hiddenDiv.find('select option');
+                        describe("should be filled with the existing hidden select options", function () {
+                            var datalistOptions;
+                            var hiddenSelectOptions;
 
-                            expect(datalistOptions).toHaveLength(hiddenSelectOptions.length);
-
-                            var hiddenOptionValues = hiddenSelectOptions.map(function() {
-                                return $(this).val();
+                            beforeEach(function () {
+                                datalistOptions = datalist.find('option');
+                                hiddenSelectOptions = hiddenDiv.find('select option');
                             });
 
-                            datalistOptions.each(function(index, datalistOption) {
-                                expect(hiddenOptionValues).toContain(datalistOption.value);
+                            it("should have the same length", function () {
+                                expect(datalistOptions).toHaveLength(hiddenSelectOptions.length);
                             });
+
+                            it("should contains the same values", function () {
+                                var hiddenOptionValues = hiddenSelectOptions.map(function () {
+                                    return $(this).val();
+                                });
+
+                                datalistOptions.each(function (index, datalistOption) {
+                                    expect(hiddenOptionValues).toContain(datalistOption.value);
+                                });
+                            });
+
+                            it("should have data-id with the index of the hidden select option", function () {
+                                datalistOptions.each(function (index, datalistOption) {
+                                    expect(datalistOption).toHaveData('id', index);
+                                });
+                            });
+
+                        });
+                    });
+                });
+            });
+
+            describe("a new div contains the descrition for a11y screen reader", function () {
+                var srDiv;
+                var hiddenDiv;
+
+                beforeEach(function () {
+                    hiddenDiv = tranformedForm.find('div:hidden');
+                    srDiv = hiddenDiv.prev().prev();
+                });
+
+                it("should add the div before the a11y enhanced selector div", function () {
+                    expect(srDiv).toExist();
+                    expect(srDiv).toBeMatchedBy('div');
+                });
+
+                it("should have a class sr-only", function () {
+                    expect(srDiv).toHaveClass('sr-only');
+                });
+
+                it("should contain a paragraph", function () {
+                    expect(srDiv.find('p')).toExist();
+                });
+
+                it("should have an attribut 'aria-live'", function () {
+                    expect(srDiv).toHaveAttr('aria-live', 'polite');
+                });
+            });
+
+            describe("a list contains the selected items", function () {
+                var ulSelection;
+
+                beforeEach(function () {
+                    ulSelection = hiddenDiv.next();
+                });
+
+                it("should add the ul after the hidden div", function () {
+                    expect(ulSelection).toExist();
+                    expect(ulSelection).toBeMatchedBy('ul');
+                });
+
+                it("should have a class list-inline", function () {
+                    expect(ulSelection).toHaveClass('list-inline');
+                });
+
+                describe("on selection of an item in a11y selector", function () {
+                    var listItem;
+
+                    beforeEach(function () {
+                        a11yDiv.find('#a11y-select-js').val('Option 2').trigger('input');
+                        listItem = ulSelection.find('li');
+                    });
+
+                    it("should add a list item in the selected items list", function () {
+                        expect(listItem.length).toEqual(1);
+                        expect(listItem).toContainText('Option 2');
+                    });
+
+                    describe("the added list item", function () {
+                        it("should have a span which references the hidden selected option", function () {
+                            var span = listItem.find('span');
+                            expect(span).toExist();
+                            expect(span).toHaveId('selected-item-1');
+                            expect(span).toHaveClass('tag-item');
+                        });
+
+                        it("should have a button to permit the deletion", function () {
+                            var button = listItem.find('span button');
+                            expect(button).toExist();
+                            expect(button).toHaveClass('tag-item-supp');
+                            expect(button).toHaveAttr('title', 'supprimer la selection');
+                            expect(button).toHaveAttr('aria-labelledby', 'selected-item-1');
+                            expect(button).toHaveAttr('type', 'button');
+                        });
+
+                        it("should have a span in the button for the screen reader", function () {
+                            var innerSpan = listItem.find('span button span');
+                            expect(innerSpan).toExist();
+                            expect(innerSpan).toHaveClass('sr-only');
+                            expect(innerSpan).toHaveText('supprimer');
                         });
                     });
 
+                    describe("when the selected element is already selected", function () {
+                        beforeEach(function () {
+                            a11yDiv.find('#a11y-select-js').val('Option 2').trigger('input');
+                            listItem = ulSelection.find('li');
+                        });
+
+                        it("should not add the item to the list", function () {
+                            expect(listItem.length).toEqual(1);
+                            expect(listItem).toContainText('Option 2');
+                        });
+
+                    });
                 });
-
-
-            });
-
-
+            })
         });
 
     });
